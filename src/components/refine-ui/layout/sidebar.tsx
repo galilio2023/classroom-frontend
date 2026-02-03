4fdba37ca22539656ae90efe24808f313d0a9ebf
ibrahim galal <ibrahimgalal2011@gmail.com>
2026-02-03 09:16:09 +0200

    remove unused code

--- src/components/refine-ui/layout/sidebar.tsx
+++ src/components/refine-ui/layout/sidebar.tsx
@@ -241,68 +241,6 @@ function SidebarHeader() {
     </ShadcnSidebarHeader>
   );
 }
-// function SidebarHeader() {
-//   const { title } = useRefineOptions();
-//   const { open, isMobile } = useShadcnSidebar();
-//
-//   return (
-//     <ShadcnSidebarHeader
-//       className={cn(
-//         "p-0",
-//         "h-16",
-//         "border-b",
-//         "border-border",
-//         "flex-row",
-//         "items-center",
-//         "justify-between",
-//         "overflow-hidden"
-//       )}
-//     >
-//       <div
-//         className={cn(
-//           "whitespace-nowrap",
-//           "flex",
-//           "flex-row",
-//           "h-full",
-//           "items-center",
-//           "justify-start",
-//           "gap-2",
-//           "transition-discrete",
-//           "duration-200",
-//           {
-//             "pl-3": !open,
-//             "pl-5": open,
-//           }
-//         )}
-//       >
-//         <div>{title.icon}</div>
-//         <h2
-//           className={cn(
-//             "text-sm",
-//             "font-bold",
-//             "transition-opacity",
-//             "duration-200",
-//             {
-//               "opacity-0": !open,
-//               "opacity-100": open,
-//             }
-//           )}
-//         >
-//           {title.text}
-//         </h2>
-//       </div>
-//
-//       <ShadcnSidebarTrigger
-//         className={cn("text-muted-foreground", "mr-1.5", {
-//           "opacity-0": !open,
-//           "opacity-100": open || isMobile,
-//           "pointer-events-auto": open || isMobile,
-//           "pointer-events-none": !open && !isMobile,
-//         })}
-//       />
-//     </ShadcnSidebarHeader>
-//   );
-// }
 
 function getDisplayName(item: TreeMenuItem) {
   return item.meta?.label ?? item.label ?? item.name;
